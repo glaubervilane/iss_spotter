924359607b83030c77126e4bd0c0e104897ca7fb
Glauber Marques <glauberfmarques@gmail.com>
2023-05-22 09:29:34 -0600

    Adding the same logic in ISS_Spotter application but using promises this time

---
+++ iss_promised.js
@@ -0,0 +1,28 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => JSON.parse(data))
+    .catch((error) => {
+      throw new Error(`Error: ${error.message}`);
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
